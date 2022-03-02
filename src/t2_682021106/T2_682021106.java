5543be279305cf3b2ebdc06b849ed9548201b7f9
azizzzzf <100188723+azizzzzf@users.noreply.github.com>
2022-03-02 08:07:58 +0700

    Add files via upload

---
+++ src/t2_682021106/T2_682021106.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t2_682021106;
+
+/**
+ *
+ * @author O M E N
+ */
+public class T2_682021106 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 12; //BA = a
+        int b = 12; //AD = b
+        int c = 22; //DC = c
+        int d = 22; //CB = d
+        int p = 15; //BD = p
+        int t = 30; //AC = t
+        int keliling = a+b+c+d;
+        int luas = p*t/2;
+        System.out.println("keliling = a+b+c+d = " + keliling);
+        System.out.println("luas = p*t/2 = " + luas);
+    }
+    
+}
